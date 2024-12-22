6164b99f9f170e0fc36c12b4002d4582627dc71f
Nure <nalamsiddiq@gmail.com>
2024-12-22 21:46:42 +0600

    Added lonting and formatting

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 # flake8, black
 
-# def sum():
 
-#     a = 5
-#     b = 10
-#     return a + b
-
-
-# sum()
+def sum():
+    a = 5
+    b = 10
+    return a + b
 
 
-def add(a, b): return a + b
-result = add(5 + 10); print(result)
+sum()
+
+
+def add(a, b):
+    return a + b
+
+
+result = add(5 + 10)
+print(result)
